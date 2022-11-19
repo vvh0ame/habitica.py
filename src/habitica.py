99f0c1d4a5b9b4c2264d1b6f034f731c33ecea91
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:54:46 +0800

    Update habitica.py

--- src/habitica.py
+++ src/habitica.py
@@ -1,7 +1,7 @@
 import requests
 
 class Habitica:
-	def __init__(self, language: str = "en"):
+	def __init__(self, language: str = "en") -> None:
 		self.api = "https://habitica.com/api"
 		self.headers = {
 			"user-agent": "Dalvik/2.1.0 (Linux; U; Android 7.1.2; ASUS_Z01QD Build/QKQ1.190825.002)",
@@ -10,7 +10,7 @@ class Habitica:
 		}
 		self.language = language
 
-	def login(self, username: str, password: str):
+	def login(self, username: str, password: str) -> dict:
 		data = {
 			"password": password,
 			"username": username
@@ -30,7 +30,7 @@ class Habitica:
 			self,
 			email: str,
 			password: str,
-			username: str):
+			username: str) -> dict:
 		data = {
 			"confirmPassword": password,
 			"email": email,
@@ -42,37 +42,37 @@ class Habitica:
 			data=data,
 			headers=self.headers).json()
 
-	def get_current_user(self):
+	def get_current_user(self) -> dict:
 		return requests.get(
 			f"{self.api}/v4/user",
 			headers=self.headers).json()
 
-	def get_app_rewards(self):
+	def get_app_rewards(self) -> dict:
 		return requests.get(
 			f"{self.api}/v4/user/in-app-rewards",
 			headers=self.headers).json()
 
-	def get_world_state(self):
+	def get_world_state(self) -> dict:
 		return requests.get(
 			f"{self.api}/v4/world-state",
 			headers=self.headers).json()
 
-	def get_group_plans(self):
+	def get_group_plans(self) -> dict:
 		return requests.get(
 			f"{self.api}/v4/group-plans",
 			headers=self.headers).json()
 
-	def get_tasks(self):
+	def get_tasks(self) -> dict:
 		return requests.get(
 			f"{self.api}/v4/tasks/user",
 			headers=self.headers).json()
 
-	def get_conversations(self):
+	def get_conversations(self) -> dict:
 		return requests.get(
 			f"{self.api}/v4/inbox/conversations",
 			headers=self.headers).json()
 
-	def update_username(self, username: str):
+	def update_username(self, username: str) -> dict:
 		data = {
 			"username": username
 		}
@@ -81,7 +81,7 @@ class Habitica:
 			data=data,
 			headers=self.headers).json()
 
-	def update_email(self, email: str, password: str):
+	def update_email(self, email: str, password: str) -> dict:
 		data = {
 			"password": password,
 			"newEmail": email
@@ -94,7 +94,7 @@ class Habitica:
 	def update_password(
 			self,
 			old_password: str,
-			new_password: str):
+			new_password: str) -> dict:
 		data = {
 			"confirmPassword": new_password,
 			"newPassword": new_password,
@@ -118,7 +118,7 @@ class Habitica:
 			avatar_hair_color: str = None,
 			avatar_hair_base: int = None,
 			avatar_hair_bangs: int = None,
-			avatar_chair_color: str = None):
+			avatar_chair_color: str = None) -> dict:
 		data = {}
 		if profile_name:
 			data["profile.name"] = profile_name
@@ -149,17 +149,17 @@ class Habitica:
 			data=data,
 			headers=self.headers).json()
 
-	def get_market_gear(self):
+	def get_market_gear(self) -> dict:
 		return requests.get(
 			f"{self.api}/v4/shops/market-gear?lang={self.language}",
 			headers=self.headers).json()
 
-	def get_market(self):
+	def get_market(self) -> dict:
 		return requests.get(
 			f"{self.api}/v4/shops/market?lang={self.language}",
 			headers=self.headers).json()
 
-	def buy_item(self, item_name: str):
+	def buy_item(self, item_name: str) -> dict:
 		data = {
 			"quantity": 1
 		}
@@ -168,17 +168,17 @@ class Habitica:
 			data=data,
 			headers=self.headers).json()
 
-	def get_quests(self):
+	def get_quests(self) -> dict:
 		return requests/get(
 			f"{self.api}/v4/shops/quests?lang={self.language}",
 			headers=self.headers).json()
 
-	def get_time_travelers(self):
+	def get_time_travelers(self) -> dict:
 		return requests.get(
 			f"{self.api}/v4/shops/time-travelers?lang={self.language}",
 			headers=self.headers).json()
 
-	def equip_item(self, item_name: str):
+	def equip_item(self, item_name: str) -> dict:
 		return requests.post(
 			f"{self.api}/v4/user/equip/equipped/{item_name}",
 			headers=self.headers).json()
@@ -189,7 +189,7 @@ class Habitica:
 			description: str,
 			type: str = "party",
 			only_leader_challenges: bool = False,
-			only_leader_gets_gems: bool = False):
+			only_leader_gets_gems: bool = False) -> dict:
 		data = {
 			"name": name,
 			"description": description,
@@ -205,17 +205,17 @@ class Habitica:
 			data=data,
 			headers=self.headers).json()
 
-	def get_group_info(self, group_id: str):
+	def get_group_info(self, group_id: str) -> dict:
 		return requests.get(
 			f"{self.api}/v4/groups/{group_id}",
 			headers=self.headers).json()
 
-	def get_group_chat(self, group_id: str):
+	def get_group_chat(self, group_id: str) -> dict:
 		return requests.get(
 			f"{self.api}/v4/groups/{group_id}/chat",
 			headers=self.headers).json()
 	
-	def send_message(self, group_id: str, message: str):
+	def send_message(self, group_id: str, message: str) -> dict:
 		data = {
 			"message": message
 		}
@@ -224,7 +224,7 @@ class Habitica:
 			data=data,
 			headers=self.headers).json()
 
-	def delete_message(self, group_id: str, message_id: str):
+	def delete_message(self, group_id: str, message_id: str) -> dict:
 		return requests.delete(
 			f"{self.api}/v4/groups/{group_id}/chat/{message_id}",
 			headers=self.headers).json()
@@ -236,7 +236,7 @@ class Habitica:
 			description: str = None,
 			type: str = "party",
 			only_leader_challenges: bool = False,
-			only_leader_gets_gems: bool = False):
+			only_leader_gets_gems: bool = False) -> dict:
 		data = {}
 		if name:
 			data["name"] = name
@@ -253,17 +253,20 @@ class Habitica:
 			data=data,
 			headers=self.headers).json()
 
-	def leave_from_group(self, group_id: str):
+	def leave_from_group(self, group_id: str) -> dict:
 		return requests.post(
 			f"{self.api}/v4/groups/{group_id}/leave?keepChallenges=leave-challenges",
 			headers=self.headers).json()
 
-	def get_user_profile(self, user_id: str):
+	def get_user_profile(self, user_id: str) -> dict:
 		return requests.get(
 			f"{self.api}/v4/members/{user_id}",
 			headers=self.headers).json()
 
-	def send_private_message(self, user_id: str, message: str):
+	def send_private_message(
+			self,
+			user_id: str,
+			message: str) -> dict:
 		data = {
 			"message": message,
 			"toUserId": user_id
@@ -273,32 +276,38 @@ class Habitica:
 			data=data,
 			headers=self.headers).json()
 
-	def get_conversation_messages(self, conversation_id: str, page: int = 0):
+	def get_conversation_messages(
+			self,
+			conversation_id: str,
+			page: int = 0) -> dict:
 		return requests.get(
 			f"{self.api}/v4/inbox/messages?conversation={conversation_id}&page={page}",
 			headers=self.headers).json()
 
-	def block_user(self, user_id: str):
+	def block_user(self, user_id: str) -> dict:
 		return requests.post(
 			f"{self.api}/v4/user/block/{user_id}",
 			headers=self.headers).json()
 
-	def get_challenges(self, page: int = 0):
+	def get_challenges(self, page: int = 0) -> dict:
 		return requests.get(
 			f"{self.api}/v4/challenges/user?page={page}",
 			headers=self.headers).json()
 
-	def get_challenge_info(self, challenge_id: str):
+	def get_challenge_info(self, challenge_id: str) -> dict:
 		return requests.get(
 			f"{self.api}/v4/challenges/{challenge_id}",
 			headers=self.headers).json()
 
-	def join_challenge(self, challenge_id: str):
+	def join_challenge(self, challenge_id: str) -> dict:
 		return requests.post(
 			f"{self.api}/v4/challenges/{challenge_id}/join",
 			headers=self.headers).json()
 
-	def leave_challenge(self, challenge_id: str, keep: str = "remove-all"):
+	def leave_challenge(
+			self,
+			challenge_id: str,
+			keep: str = "remove-all") -> dict:
 		data = {
 			"keep": keep
 		}
@@ -307,17 +316,19 @@ class Habitica:
 			data=data,
 			headers=self.headers).json()
 
-	def get_user_achievements(self, user_id: str):
+	def get_user_achievements(
+			self,
+			user_id: str) -> dict:
 		return requests.get(
 			f"{self.api}/v4/members/{user_id}/achievements?lang={self.language}",
 			headers=self.headers).json()
 
-	def reset_account(self):
+	def reset_account(self) -> dict:
 		return requests.post(
 			f"{self.api}/v4/user/reset",
 			headers=self.headers).json()
 
-	def delete_account(self, password: str):
+	def delete_account(self, password: str) -> dict:
 		data = {
 			"password": password
 		}
@@ -326,7 +337,7 @@ class Habitica:
 			data=data,
 			headers=self.headers).json()
 	
-	def get_status(self):
+	def get_status(self) -> dict:
 		return requests.get(
 			f"{self.api}/v4/status",
 			headers=self.headers).json()
